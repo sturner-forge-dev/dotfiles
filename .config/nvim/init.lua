71736ceb833f4f0ba7876048d36247af6caadd3e
Steven Turner <sturner@forge-applications.io>
2025-01-19 18:34:12 -0600

    adding nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- temporary Treesitter Inspect cmd workaround - github.com/neovim/neovim/issues/31675
+vim.hl = vim.highlight
+
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
