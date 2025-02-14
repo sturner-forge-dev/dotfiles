65ae86f94d28827acc27a073fcac280e84c9a014
stevent12-rg <sturner@rategenius.com>
2025-02-14 15:42:23 -0600

    playing with dashboard

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -33,7 +33,7 @@ return {
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
         -- Used by the `header` section
@@ -74,6 +74,7 @@ return {
       },
       sections = {
         { section = "header" },
+        { title = "DON'T SACRIFICE ADEPTNESS FOR HIGHER VELOCITY.", indent = 8, padding = 1 },
         { icon = "  ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         {
