e1ecc79b4e7d7ddd5153d21a74e5814f1f6c81bb
stevent12-rg <sturner@rategenius.com>
2025-06-02 14:31:22 -0600

    delete mason workaround / add gitsigns keymaps / modify borders plugin

--- nvim-kickstart/.config/nvim-kickstart/lua/custom/plugins/vim-fugitive.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/custom/plugins/vim-fugitive.lua
@@ -1,6 +1,12 @@
 return {
   'tpope/vim-fugitive',
   keys = {
-    { '<leader>gi', ':Git<CR>', noremap = true, silent = true, desc = 'Git Fugitive: Open Git Interface' },
+    {
+      '<leader>gi',
+      ':Git<CR>',
+      noremap = true,
+      silent = true,
+      desc = 'Git Fugitive: Open [G]it [I]nterface',
+    },
   },
 }
