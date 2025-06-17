562041e5101358cfdc9d5a7d11e0313be1965a65
stevent12-rg <sturner@rategenius.com>
2025-06-17 15:33:15 -0600

    edit harpoon keymaps

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -17,19 +17,19 @@ return {
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Op[e]n Harpoon' })
 
-    vim.keymap.set('n', '<C-1>', function()
+    vim.keymap.set('n', '<M-1>', function()
       harpoon:list():select(1)
     end, { desc = 'Harpoon to Buffer 1' })
 
-    vim.keymap.set('n', '<C-2>', function()
+    vim.keymap.set('n', '<M-2>', function()
       harpoon:list():select(2)
     end, { desc = 'Harpoon to Buffer 2' })
 
-    vim.keymap.set('n', '<C-3>', function()
+    vim.keymap.set('n', '<M-3>', function()
       harpoon:list():select(3)
     end, { desc = 'Harpoon to Buffer 3' })
 
-    vim.keymap.set('n', '<C-4>', function()
+    vim.keymap.set('n', '<M-4>', function()
       harpoon:list():select(4)
     end, { desc = 'Harpoon to Buffer 4' })
 
