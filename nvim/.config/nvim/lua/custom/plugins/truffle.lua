7795bf8228883fce5ed6e46ce268e2409c57be7d
stevent12-rg <sturner@rategenius.com>
2025-08-22 15:18:10 -0600

    update truffle config / remove arrow warnings in keymaps.lua

--- nvim/.config/nvim/lua/custom/plugins/truffle.lua
+++ nvim/.config/nvim/lua/custom/plugins/truffle.lua
@@ -2,7 +2,12 @@ return {
   'Nkr1shna/truffle.nvim',
   config = function()
     require('truffle').setup {
-      command = 'cursor-agent', -- REQUIRED: set your CLI command, make sure it is available in path
+      profiles = {
+        cursor = {
+          command = 'cursor-agent', -- REQUIRED: set your CLI command, make sure it is available in path
+          default = true,
+        },
+      },
       -- Layout & size
       side = 'right', -- 'right' | 'bottom' | 'left' (default: 'right')
       size = '33%', -- number (cols/rows) or percentage string like '33%'
@@ -18,7 +23,6 @@ return {
         toggle = '<leader>ag',
         send_selection = '<leader>ats',
         send_file = '<leader>atf',
-        send_input = '<leader>ati',
       },
     }
   end,
